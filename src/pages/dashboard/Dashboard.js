38581c910944cec2f67681e28d755eff647ecd89
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-25 22:32:22 +0100

    populating homepage

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,24 @@
+import { useCollection } from '../../hooks/useCollection'
+
 // styles
 import './Dashboard.css'
 
 export default function Dashboard() {
+  const { documents, isPending, error } = useCollection('projects')
+
+  if (error) {
+    return <div className='error'>{error}</div>
+  }
+  if (isPending) {
+    return <h2>loading...</h2>
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2 className='page-title'>Dashboard</h2>
+      {documents && documents.map(project => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
   )
 }
