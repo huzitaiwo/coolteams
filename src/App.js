9da02a2d2b87cbab95ff1da3028ca9c684ead7d5
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-27 11:23:49 +0100

    setup routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch as Routes, Route } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Project from './pages/Project'
 import Task from './pages/Task'
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route path='/task'>
             <Task />
+          </Route>
+          <Route path='/projects/:id'>
+            <Project />
           </Route>
         </Routes>
       </BrowserRouter>
