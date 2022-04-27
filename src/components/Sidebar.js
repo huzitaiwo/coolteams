8350bb954211eeffbfdb943a7d72355cac4f7009
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-27 19:51:05 +0100

    added icon to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,12 @@ import React from 'react'
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-      <div className="logo">Logo here</div>
+      <div className="logo">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
+        </svg>
+      </div>
       <nav>
         <ul className="navigation-links">
           <li>
