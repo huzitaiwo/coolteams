725c74f67e15ee2f8c57c13c8c7742d4a1b0bb87
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-27 11:22:12 +0100

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import { BrowserRouter, Switch as Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch as Routes, Route } from 'react-router-dom'
 
 // styles 
 import './App.css'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Task from './pages/Task'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route exact path='/'>
+            <Dashboard />
+          </Route>
+          <Route path='/project'>
+            <Projects />
+          </Route>
+          <Route path='/task'>
+            <Task />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
