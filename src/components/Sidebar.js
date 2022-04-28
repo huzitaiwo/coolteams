6858c65b09142b36c634e81c7ebf26af852334f2
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-28 21:26:17 +0100

    styled sidebar dark mode

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
 
 // styles
 import './Sidebar.css'
@@ -6,9 +7,10 @@ import './Sidebar.css'
 import React from 'react'
 
 export default function Sidebar() {
+  const { mode } = useTheme()
 
   return (
-    <div className='sidebar'>
+    <div className={`sidebar ${mode}`}>
       <div className="logo">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
