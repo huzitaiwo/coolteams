1e70ea22004976493a32d7886d99e02e90b7834b
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-28 21:29:28 +0100

    styled sidebar dark mode

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,14 +11,14 @@ export default function Sidebar() {
 
   return (
     <div className={`sidebar ${mode}`}>
-      <div className="logo">
+      <div className={`logo ${mode}`}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
           <path strokeLinecap="round" strokeLinejoin="round" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
         </svg>
       </div>
       <nav>
-        <ul className="navigation-links">
+        <ul className={`navigation-links ${mode}`}>
           <li>
             <NavLink exact to="/">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
