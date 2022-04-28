8b453b7bf9b4984bfe3aafb01d87e26fc04e75d9
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-28 05:34:15 +0100

    created ThemeSelector hook

---
+++ src/components/ThemeSelector.js
@@ -0,0 +1,21 @@
+import { useTheme } from "../hooks/useTheme"
+import lightIcon from '../assets/icon/light.svg'
+import darkIcon from '../assets/icon/dark.svg'
+
+export default function ThemeSelector() {
+  const { changeMode, mode } = useTheme()
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark')
+  }
+
+  return (
+    <div className="theme-selector">
+      <button onClick={toggleMode} className="mode-toggler">
+        <img
+          src={lightIcon} 
+          alt="mode toggler" />
+      </button>
+    </div>
+  )
+}
