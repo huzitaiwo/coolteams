a85b827e06c3f7b6a765ab98efb6416840379b2a
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-29 04:57:21 +0100

    Auth context hook

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -16,7 +16,7 @@ export const authReducer = (state, action) => {
   }
 }
 
-export const AuthContextProvider = ({ children }) => {
+export const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
     authIsReady: false
