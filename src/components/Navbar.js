fb9e6d4d86e618b16e7f92457f4e5a32ccff940f
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-29 10:19:50 +0100

    used Avatar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { useTheme } from '../hooks/useTheme'
+import { useAuthContext } from '../hooks/useAuthContext'
 import { useLogout } from '../hooks/useLogout'
 
-// styles
+// styles and components
 import './Navbar.css'
+import Avatar from './Avatar'
 
 export default function Navbar() {
+  const { user } = useAuthContext()
   const { mode } = useTheme()
   const { logout, isPending, error } = useLogout()
 
@@ -18,9 +21,7 @@ export default function Navbar() {
           <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
         </svg>
       </button>
-      <div className="avatar">
-        <img src="./images/person1.png" alt="" />
-      </div>
+      <Avatar src={user.photoURL}/>
       
       {!isPending && <button onClick={logout} className={`btn ${mode}`}>Logout</button>}
       {isPending && <button onClick={logout} className={`btn ${mode}`}>Loging out...</button>}
