e6db18ef89253fb33468990ce554393b1c73618b
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-30 21:43:28 +0100

    created form states

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -2,7 +2,17 @@
 import './Create.css'
 
 export default function Create() {
+  // form field values
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
+
   return (
-    <div>Create</div>
+    <div className='create-form'>
+      <h2 className='page-title'>Create a new project</h2>
+    </div>
   )
 }
