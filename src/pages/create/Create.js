0e707485044a0fef3f12a038117dc1834db7cd48
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-30 21:54:52 +0100

    creating add project form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -15,38 +15,50 @@ export default function Create() {
   return (
     <div className='create-form'>
       <h2 className='page-title'>Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+        </label>
 
-      <label>
-        <span>Project name:</span>
-        <input
-          required
-          type="text"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-      </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            required
+            type="text"
+            onChange={e => setDetails(e.target.value)}
+            value={details}
+          >
+          </textarea>
+        </label>
 
-      <label>
-        <span>Project details:</span>
-        <textarea
-          required
-          type="text"
-          onChange={e => setDetails(e.target.value)}
-          value={details}
-        >
-        </textarea>
-      </label>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="date"
+            onChange={e => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
 
-      <label>
-        <span>Project name:</span>
-        <input
-          required
-          type="date"
-          onChange={e => setDueDate(e.target.value)}
-          value={dueDate}
-        />
-      </label>
+        <label>
+          <span>Project Category:</span>
+          {/* category select here */}
+        </label>
 
+        <label>
+          <span>Assign to:</span>
+          {/* category select here */}
+        </label>
+
+        <button className='btn'>Add</button>
+      </form>
     </div>
   )
 }
