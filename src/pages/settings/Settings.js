8b613612122b9591506ac45e0944c16545e9236c
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-01 09:45:22 +0100

    styled setting header

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -1,14 +1,18 @@
-// components
+import { useLogout } from '../../hooks/useLogout'
+
+// styles && components
+import './Settings.css'
 import ThemeSelector from '../../components/ThemeSelector'
 
-// styles
-import './Settings.css'
-
 export default function Settings() {
+  const { logout, isPending } = useLogout()
+
   return (
     <div>
       <div className="settings-header">
         <ThemeSelector />
+        {!isPending && <button onClick={logout} className="btn">Logout</button>}
+        {isPending && <button className='btn'>Logging out...</button>}
       </div>
     </div>
   )
