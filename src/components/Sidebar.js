0f951569e93e8ee9fd1d6d9e153e58cc8fd6d46c
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-02 23:11:26 +0100

    cleanup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -60,7 +60,7 @@ export default function Sidebar() {
               Settings
             </NavLink>
           </li>
-          <li>
+          {/* <li>
             <NavLink to="/login">
               <svg xmlns="http://www.w3.org/2000/svg" className="w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
@@ -75,7 +75,7 @@ export default function Sidebar() {
               </svg>
               Signup
             </NavLink>
-          </li>
+          </li> */}
           <li>
             <NavLink to="/create">
               <svg xmlns="http://www.w3.org/2000/svg" className="w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
