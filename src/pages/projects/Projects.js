f32c23e967e89f5f96828b4b20dd7a8dcc2231d5
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-03 17:03:14 +0100

    listing all projects

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,6 +1,18 @@
 
 export default function Projects() {
+  const { mode } = useTheme()
+  const { documents, isPending, error } = useCollection('projects')
+
+  if (error) {
+    return <div className='error'>{error}</div>
+  }
+  if (isPending) {
+    return <h4>loading...</h4>
+  }
+
   return (
-    <div>Projects</div>
+    <div>
+      {documents && <ProjectList projects={documents} />}
+    </div>
   )
 }
