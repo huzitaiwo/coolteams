d2d23101bef9c134942b889ef9872f5803fae686
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-05-09 18:14:36 +0100

    restructuring project cards

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -17,24 +17,26 @@ export default function ProjectList({ projects }) {
       {projects.map(project => (
         <Link className='card' to={ `/projects/${project.id}`} key={project.id}>
           <img className='project-image' src={project.photoURL} alt="placeholder" />
-          <h4>{project.name}</h4>
-          <p>These project will need a brand new identity.</p>
-          <div className='assigned-to'>
-            <div className="list">
-              <small>{project.assignedUsersList.length}</small>
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-              </svg>
+          <div className="card-body">
+            <h4>{project.name}</h4>
+            <p>These project will need a brand new identity.</p>
+            <div className='assigned-to'>
+              <div className="list">
+                <small>{project.assignedUsersList.length}</small>
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                </svg>
+              </div>
+              <ul className="users">
+                {project.assignedUsersList.map(user => (
+                  <li key={user.photoURL}>
+                    <Avatar src={user.photoURL} />
+                  </li>
+                ))}
+              </ul>
             </div>
-            <ul className="users">
-              {project.assignedUsersList.map(user => (
-                <li key={user.photoURL}>
-                  <Avatar src={user.photoURL} />
-                </li>
-              ))}
-            </ul>
+            <p className='success'>Due by: {project.dueDate.toDate().toDateString()}</p>
           </div>
-          <p className='success'>Due by: {project.dueDate.toDate().toDateString()}</p>
         </Link>
       ))}
     </div>
