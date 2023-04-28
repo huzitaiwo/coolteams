1b1d35d4b494f869293c6653a780a781ccf898b0
huzitaiwo <taiwohusayn5@gmail.com>
2023-04-28 15:59:04 +0100

    creating project comments

---
+++ src/pages/project/Comment.js
@@ -0,0 +1,73 @@
+import { useState } from "react"
+import { timestamp } from "../../firebase/config"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useFirestore } from '../../hooks/useFirestore'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+//components
+import Avatar from "../../components/Avatar"
+
+const Comment = ({ project }) => {
+  const [newComment, setNewComment ] = useState('')
+  const { user } = useAuthContext()
+  const { updateDocument, response } = useFirestore('projects')
+
+  const handleSubmit =  async (e) => {
+    e.preventDefault()
+
+    const comment = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      content: newComment,
+      createdAt: timestamp.fromDate(new Date()),
+      id: Math.random()
+    }
+
+    await updateDocument(project.id, {
+      comments: [ ...project.comments, comment ]
+    })
+
+    if (!response.error) {
+      setNewComment('')
+    }
+  }
+
+  return (
+    <div className="project-comments">
+      <h4>Project comments</h4>
+
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL} />
+            <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>{formatDistanceToNow(comment.createdAt.toDate(), { addSuffix: true })}</p>
+            </div>
+            <div className="commnet-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit} className="add-comment">
+        <label>
+          <span>Add new comment</span>
+          <textarea
+            required
+            onChange={e => {setNewComment(e.target.value)}}
+            value={newComment}          
+          ></textarea>
+          {!response.isLoading && <button className="btn">send</button>}
+          {response.isLoading && <button className="btn">sending...</button>}
+          {response.error && <div className="error">{response.error}</div>}
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default ProjectComment
