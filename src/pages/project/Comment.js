6e3d1370bd9eafdac69d8363787171df6c110552
huzitaiwo <taiwohusayn5@gmail.com>
2023-04-29 11:04:56 +0100

    completed project comments functionality

--- src/pages/project/Comment.js
+++ src/pages/project/Comment.js
@@ -5,6 +5,7 @@ import { useState } from "react"
 import { timestamp } from "../../firebase/config"
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { useFirestore } from '../../hooks/useFirestore'
+import { useTheme } from "../../hooks/useTheme"
 
 // external packages
 import formatDistanceToNow from "date-fns/formatDistanceToNow"
@@ -14,9 +15,13 @@ import { v4 as uuid } from "uuid"
 import Avatar from "../../components/Avatar"
 
 const Comment = ({ project }) => {
+  const { mode } = useTheme()
   const [newComment, setNewComment ] = useState('')
   const { user } = useAuthContext()
   const { updateDocument, response } = useFirestore('projects')
+  const assignedUsers = project.assignedUsersList.map(u => {
+    return u.uid
+  })
 
   console.log(user, project.comments)
 
@@ -40,22 +45,24 @@ const Comment = ({ project }) => {
     }
   }
 
+  console.log(assignedUsers)
+
   return (
-    <div className="project__comments project__card">
-      <h3>Project comments</h3>
+    <div className={`project__comments ${mode}`}>
+      <h3>Comments</h3>
 
       <ul>
         {project.comments && project.comments.map(comment => (
           <li key={comment.id}>
-            <div className="comment__author">
+            <div className="comment">
               <Avatar src={comment.photoURL} />
-            <p>{comment.displayName}</p>
-            </div>
-            <div className="comment__date">
-              <p>{formatDistanceToNow(comment.createdAt.toDate(), { addSuffix: true })}</p>
-            </div>
-            <div className="commnet__content">
-              <p>{comment.content}</p>
+              <div>
+                <div className="comment__author">
+                  <p>{comment.displayName}</p>
+                  <small className="comment__time">{formatDistanceToNow(comment.createdAt.toDate(), { addSuffix: true })}</small>
+                </div>
+                <p className="comment__text">{comment.content}</p>
+              </div>
             </div>
           </li>
         ))}
@@ -63,14 +70,26 @@ const Comment = ({ project }) => {
 
       <form onSubmit={handleSubmit} className="add__comment">
         <label>
-          <span>Add new comment</span>
           <textarea
             required
             onChange={e => {setNewComment(e.target.value)}}
             value={newComment}          
           ></textarea>
-          {!response.isLoading && <button className="btn">Add comment</button>}
-          {response.isLoading && <button className="btn">Adding comment...</button>}
+          {!response.isLoading && (
+            <button className={`send ${mode}`}>
+              <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_576_43)">
+                <path d="M18.65 31.3778L13.7003 26.428L17.7072 19.1213L10.4004 23.1282L5.45068 18.1785L25.4854 11.3431L18.65 31.3778Z" fill="white"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_576_43">
+                <rect width="24" height="28" fill="white" transform="translate(0.0292969 17) rotate(-45)"/>
+                </clipPath>
+                </defs>
+              </svg>
+
+            </button>
+          )}
           {response.error && <div className="error">{response.error}</div>}
         </label>
       </form>
