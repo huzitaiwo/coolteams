75947b54ae256b9ab79cd61555020cd87e89192d
huzitaiwo <taiwohusayn5@gmail.com>
2023-05-06 17:48:39 +0100

    fixed mobile responsive bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,19 @@ export default function Navbar() {
   const { user } = useAuthContext()
   const { mode } = useTheme()
 
+
+  const handleClick = () => {
+    console.log('click')
+  }
+
   return (
     <header className={mode}>
+      <button onClick={handleClick} className="list hamburger">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+
       <form className={`search ${mode}`}>
         <input type="text" placeholder="search" />
       </form>
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -18,25 +18,40 @@ export default function Dashboard() {
   const { user } = useAuthContext()
   const { documents, isPending, error } = useCollection('projects')
 
+
+  const myProjects = documents ? documents.filter(document => {
+    let createdByMe = false
+    if (user.uid === document.createdBy.id) {
+      createdByMe = true
+    }
+    return createdByMe
+  }) : null
+
   const myTasks = documents ? documents.filter(document => {
     let assignedToMe = false
-    document.assignedUsersList.forEach(u => {
-      if (user.uid === u.id) {
-        assignedToMe = true
-      }
+    document.tasks.filter(task => {
+      task.assignedUsersList.forEach(u => {
+        if (user.uid === u.id) {
+          assignedToMe = true
+        }
+      })
     })
     return assignedToMe
   }) : null
 
-  const myProjects = documents ? documents.filter(document => {
-    let createdByMe = false
-    document.assignedUsersList.forEach(u => {
-      if (user.uid === document.createdBy.id) {
-        createdByMe = true
-      }
-    })
-    return createdByMe
-  }) : null
+  // documents && documents.forEach(doc => {
+  //   let assignedToMe = false
+  //   const myTasks = doc ? doc.tasks.filter(task => {
+  //     task.assignedUsersList.forEach(u => {
+  //       if (user.uid === u.id) {
+  //         assignedToMe = true
+  //       }
+  //     })
+  //     return assignedToMe
+  //   }) : null
+  //   console.log(myTasks)
+  // })
+
 
   if (error) {
     return <div className='error'>{error}</div>
@@ -52,12 +67,12 @@ export default function Dashboard() {
           <Header grid={true} list={false} />
           <div className="project__grid">
             <div className="my__projects">
-              <div className="project__grid-header">
+              {/* <div className="project__grid-header">
                 <h3 className={`project__status ${mode}`}>My Projects<span>({myProjects !== 0 && myProjects.length < 10 && 0}{myProjects.length})</span></h3>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                 </svg>
-              </div>
+              </div> */}
               {myProjects.length === 0 && <h3 className={`warning__text ${mode}`}>Projects you created will show here</h3>}
               {myProjects.map(project => (
                 <Link className={`project__card ${mode}`} to={`/project/${project.id}`} key={project.id}>
@@ -65,7 +80,7 @@ export default function Dashboard() {
                 </Link>
               ))}
             </div>
-            <div className="my__task">
+            {/* <div className={`my__task ${mode}`}>
               <div className="project__grid-header">
                 <h3 className={`project__status ${mode}`}>My Tasks<span>({myTasks !== 0 && myTasks.length < 10 && 0}{myTasks.length})</span></h3>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -73,12 +88,17 @@ export default function Dashboard() {
                 </svg>
               </div>
               {myTasks.length === 0 && <h3 className={`warning__text ${mode}`}>Projects you asigned to will show here</h3>}
-              {myTasks.map(project => (
-                <Link className={`project__card ${mode}`} to={`/project/${project.id}`} key={project.id}>
-                  <ProjectsList project={project} />
-                </Link>
-              ))}
-            </div>
+              <ul>
+                {myTasks.map(task => (
+                  <li key={task.id}>
+                    <Link className={mode} to='/'>
+                      <span>00</span>
+                      <span>{task.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div> */}
           </div>
         </>
       )}
