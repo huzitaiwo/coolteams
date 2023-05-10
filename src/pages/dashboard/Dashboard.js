19227820a679019ed1ec6ca5e079769a9da9be60
huzi taiwo <taiwohusayn5@gmail.com>
2023-05-10 07:08:39 +0100

    fixed responsiveness bug

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,63 +1,40 @@
 // react packaged
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 // components & pages
-import ProjectsList from '../../components/ProjectsList'
-import Header from '../../components/Header'
+import ProjectsList from "../../components/ProjectsList";
+import Header from "../../components/Header";
 
 // hooks
-import { useCollection } from '../../hooks/useCollection'
-import { useAuthContext } from '../../hooks/useAuthContext'
-import { useTheme } from '../../hooks/useTheme'
+import { useCollection } from "../../hooks/useCollection";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useTheme } from "../../hooks/useTheme";
 
 // styles
-import './Dashboard.css'
+import "./Dashboard.css";
 
 export default function Dashboard() {
-  const { mode } = useTheme()
-  const { user } = useAuthContext()
-  const { documents, isPending, error } = useCollection('projects')
+  const { mode } = useTheme();
+  const { user } = useAuthContext();
+  const { documents, isPending, error } = useCollection("projects");
 
-
-  const myProjects = documents ? documents.filter(document => {
-    let createdByMe = false
-    if (user.uid === document.createdBy.id) {
-      createdByMe = true
-    }
-    return createdByMe
-  }) : null
-
-  const myTasks = documents ? documents.filter(document => {
-    let assignedToMe = false
-    document.tasks.filter(task => {
-      task.assignedUsersList.forEach(u => {
-        if (user.uid === u.id) {
-          assignedToMe = true
-        }
+  const myProjects = documents
+    ? documents.filter((document) => {
+        let assignedToMe = false;
+        document.assignedUsersList.forEach((u) => {
+          if (user.uid === u.id) {
+            assignedToMe = true;
+          }
+        });
+        return assignedToMe;
       })
-    })
-    return assignedToMe
-  }) : null
-
-  // documents && documents.forEach(doc => {
-  //   let assignedToMe = false
-  //   const myTasks = doc ? doc.tasks.filter(task => {
-  //     task.assignedUsersList.forEach(u => {
-  //       if (user.uid === u.id) {
-  //         assignedToMe = true
-  //       }
-  //     })
-  //     return assignedToMe
-  //   }) : null
-  //   console.log(myTasks)
-  // })
-
+    : null;
 
   if (error) {
-    return <div className='error'>{error}</div>
+    return <div className="error">{error}</div>;
   }
   if (isPending) {
-    return <h4>loading...</h4>
+    return <h4>loading...</h4>;
   }
 
   return (
@@ -73,9 +50,17 @@ export default function Dashboard() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                 </svg>
               </div> */}
-              {myProjects.length === 0 && <h3 className={`warning__text ${mode}`}>Projects you created will show here</h3>}
-              {myProjects.map(project => (
-                <Link className={`project__card ${mode}`} to={`/project/${project.id}`} key={project.id}>
+              {myProjects.length === 0 && (
+                <h3 className={`warning__text ${mode}`}>
+                  Projects you created will show here
+                </h3>
+              )}
+              {myProjects.map((project) => (
+                <Link
+                  className={`project__card ${mode}`}
+                  to={`/project/${project.id}`}
+                  key={project.id}
+                >
                   <ProjectsList project={project} />
                 </Link>
               ))}
@@ -103,5 +88,5 @@ export default function Dashboard() {
         </>
       )}
     </div>
-  )
+  );
 }
